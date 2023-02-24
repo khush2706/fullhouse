b0943d2ba094e6c26f36e4ba3a89ec73c8eb540f
khush2706 <kc220313@gmail.com>
2023-02-25 01:18:14 +0530

    update: update the user schema

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -3,9 +3,10 @@ const mongoose = require("mongoose");
 const User = new mongoose.Schema(
   {
     email: { type: String, required: true, min: 6, max: 255, unique: true },
-    username: { type: String, required: true, min: 6, max: 255 },
+    username: { type: String, required: true, min: 6, max: 255, unique: true },
     password: { type: String, required: true, min: 6, max: 255 },
     createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date, default: Date.now },
   },
   { collection: "user-data" }
 );
@@ -13,4 +14,3 @@ const User = new mongoose.Schema(
 const model = mongoose.model("UserData", User);
 
 module.exports = model;
-
--- backend/services/auth.register.service.js
+++ backend/services/auth.register.service.js
@@ -1,11 +1,18 @@
 const User = require("../models/user.model");
 
 const registerService = async (email, username, password, res) => {
-  // throw error when username already registered
+  // throw error when email already registered
   const isEmailExist = await User.findOne({ email: email });
 
   if (isEmailExist) {
-    return res.status(400).json({ error: "Email already exists " });
+    return res.status(400).json({ error: "Email already exists" });
+  }
+
+  // throw error when username already registered
+  const isUsernameExist = await User.findOne({ username: username });
+
+  if (isUsernameExist) {
+    return res.status(400).json({ error: "Username already exists" });
   }
 
   try {
